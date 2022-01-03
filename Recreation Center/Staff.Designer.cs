a1967e8cdca34381fba90cf156363a5fd371dc16
Kaushalgurung <kaushalgurung2057@gmail.com>
2022-01-03 10:57:06 +0545

    update

--- Recreation Center/Staff.Designer.cs
+++ Recreation Center/Staff.Designer.cs
@@ -30,12 +30,12 @@ namespace Recreation_Center
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.Visitorslbl = new System.Windows.Forms.Label();
@@ -782,23 +782,23 @@ namespace Recreation_Center
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(1379, 440);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Visitor Reports";
+            this.tabPage3.Text = "Reports";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // weeklyreportchart
             // 
             this.weeklyreportchart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
             this.weeklyreportchart.BorderlineWidth = 10;
-            chartArea2.Name = "ChartArea1";
-            this.weeklyreportchart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.weeklyreportchart.Legends.Add(legend2);
+            chartArea4.Name = "ChartArea1";
+            this.weeklyreportchart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.weeklyreportchart.Legends.Add(legend4);
             this.weeklyreportchart.Location = new System.Drawing.Point(6, 90);
             this.weeklyreportchart.Name = "weeklyreportchart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.weeklyreportchart.Series.Add(series2);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.weeklyreportchart.Series.Add(series4);
             this.weeklyreportchart.Size = new System.Drawing.Size(400, 300);
             this.weeklyreportchart.TabIndex = 13;
             this.weeklyreportchart.Text = "chart2";
@@ -822,16 +822,16 @@ namespace Recreation_Center
             // 
             // dailyreportchart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.dailyreportchart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.dailyreportchart.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.dailyreportchart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.dailyreportchart.Legends.Add(legend3);
             this.dailyreportchart.Location = new System.Drawing.Point(13, 125);
             this.dailyreportchart.Name = "dailyreportchart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.dailyreportchart.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.dailyreportchart.Series.Add(series3);
             this.dailyreportchart.Size = new System.Drawing.Size(400, 300);
             this.dailyreportchart.TabIndex = 0;
             this.dailyreportchart.Text = "dailyreportchart";
@@ -871,7 +871,7 @@ namespace Recreation_Center
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Size = new System.Drawing.Size(1379, 440);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Weekly Reports";
+            this.tabPage4.Text = "Visitors Reports";
             // 
             // groupBox4
             // 
